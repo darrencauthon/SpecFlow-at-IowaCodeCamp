626af937751e8fb1cfc1c4cead0c65a4b22de3ac
Darren Cauthon <darren@cauthon.com>
2010-11-06 05:47:24 -0500

    Setup projects.

---
+++ outside in with specflow/MvcApplication1/MvcApplication1/Global.asax.cs
@@ -0,0 +1,15 @@
+﻿using System.Web;
+using MvcTurbine.ComponentModel;
+using MvcTurbine.Unity;
+using MvcTurbine.Web;
+
+namespace MvcApplication1
+{
+    public class MvcApplication : TurbineApplication
+    {
+        public MvcApplication()
+        {
+            ServiceLocatorManager.SetLocatorProvider(() => new UnityServiceLocator());
+        }
+    }
+}
